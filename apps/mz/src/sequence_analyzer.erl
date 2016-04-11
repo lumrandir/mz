fb16b3d5dc5d594d01960c416df7c6a38b1ec41e
Konstantin Lukinskikh <lumrandir@gmail.com>
2016-04-11 17:47:21 +0500

    it's kinda working

--- apps/mz/src/sequence_analyzer.erl
+++ apps/mz/src/sequence_analyzer.erl
@@ -1,6 +1,6 @@
 -module(sequence_analyzer).
 
--export([analyze/2, resemblence_list/1, possible_values/2, precise/2, test/2]).
+-export([analyze/2]).
 
 -define(PossibleValues, [119, 18, 93, 91, 58, 107, 111, 82, 127, 123]).
 %%% Mappings
@@ -13,7 +13,7 @@
 %% 6 => 1101111 => 01101111 => 111
 %% 7 => 1010010 => 01010010 => 82
 %% 8 => 1111111 => 01111111 => 127
-%% 9 => 1111011 => 01111011 => 123 
+%% 9 => 1111011 => 01111011 => 123
 -define(Mappings, #{
   0 => 2#1110111,
   1 => 2#0010010,
@@ -27,29 +27,35 @@
   9 => 2#1111011
 }).
 
-test(0, Acc) ->
-  Acc;
-test(V, []) ->
-  L = maps:get((V div 10), ?Mappings) bor 2#0010000 bxor 2#0010000,
-  R = maps:get((V rem 10), ?Mappings) bor 2#1000000 bxor 2#1000000,
-  Acc = possible_values(L, R),
-  io:format("~p~n~p~n", [V, Acc]),
-  test(V - 1, Acc);
-test(V, Acc) ->
-  L = maps:get((V div 10), ?Mappings) bor 2#0010000 bxor 2#0010000,
-  R = maps:get((V rem 10), ?Mappings) bor 2#1000000 bxor 2#1000000,
-  P = possible_values(L, R),
-  A = precise(Acc, P),
-  io:format("~p~n~p~n~p~n", [V, P, A]),
-  test(V - 1, A).  
+analyze({L, R}, []) ->
+  case possible_values(L, R) of
+    []      -> {error, undefined};
+    [Value] -> {found, Value};
+    Values  -> {not_found, Values}
+  end;
+analyze({L, R}, CurrentState) ->
+  case precise(CurrentState, possible_values(L, R)) of
+    []         -> {error, undefined};
+    [Solution] -> {found, Solution};
+    State      -> {not_found, State}
+  end.
 
-analyze(CurrentState, NewData) ->
-  undefined.
+is_compatible({Iprev, EprevL, EprevR}, {Icur, EcurL, EcurR}) ->
+  Lprev = maps:get((Iprev div 10), ?Mappings),
+  Rprev = maps:get((Iprev rem 10), ?Mappings),
+  Lcur = maps:get((Icur div 10), ?Mappings),
+  Rcur = maps:get((Icur rem 10), ?Mappings),
+
+  (Icur =:= Iprev - 1) and
+    (((Lprev bxor EprevL) band EcurL) =:= 0) and
+    (((Rprev bxor EprevR) band EcurR) =:= 0) and
+    (((Lcur bxor EcurL) band EprevL) =:= 0) and
+    (((Rcur bxor EcurR) band EprevR) =:= 0).
 
 precise(Previous, Current) ->
   lists:filter(
-    fun({I, E1, E2}) -> 
-      lists:any(fun({Ipr, Epr1, Epr2}) -> (Ipr - 1 =:= I) and (Epr1 band E1 =/= 0) and (Epr2 band E2 =/= 0) end, Previous) 
+    fun(Cur) ->
+      lists:any(fun(Prev) -> is_compatible(Prev, Cur) end, Previous)
     end, Current
   ).
 
@@ -57,15 +63,15 @@ possible_values(Left, Right) ->
   LeftList  = resemblence_list(Left),
   RightList = resemblence_list(Right),
   lists:sort([{L1 * 10 + R1, L2, R2} || {L1, L2} <- LeftList, {R1, R2} <- RightList]).
-  
+
 resemblence_list(Value) ->
   {_, Res} = lists:foldl(
     fun(E, {I, Acc}) ->
       case Value bor E bxor E =:= 0 of
         false -> {I + 1, Acc};
-        true  -> {I + 1, [{I, Value bxor 2#1111111}|Acc]}
+        true  -> {I + 1, [{I, Value bxor E}|Acc]}
       end
     end, {0, []}, ?PossibleValues
   ),
   Res.
-    
+
