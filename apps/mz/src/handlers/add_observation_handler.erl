de6a2035e62c3accb8b66eb772437422fa3e905c
Konstantin Lukinskikh <lumrandir@gmail.com>
2016-04-25 20:45:46 +0500

    stash

--- apps/mz/src/handlers/add_observation_handler.erl
+++ apps/mz/src/handlers/add_observation_handler.erl
@@ -9,35 +9,47 @@
 
 init(_Transport, _Req, []) ->
   {upgrade, protocol, cowboy_rest}.
-  
+
 content_types_accepted(Req, State) ->
   {[
     {{<<"application">>, <<"json">>, []}, from_json}
   ], Req, State}.
-  
+
 allowed_methods(Req, State) ->
   {[<<"POST">>], Req, State}.
-  
+
 from_json(Req, State) ->
   {ok, Body, Req2} = cowboy_req:body(Req),
-  #{<<"sequence">> := SequenceId} = jsx:decode(Body, [return_maps]),
-  Resp = case sequence_exists(SequenceId) of
-    {ok, found} -> process_observation_ok_json();
-    _            -> unknown_sequence_error_json()
-  end,    
+  Resp = try
+    #{<<"sequence">> := SequenceId, <<"observation">> := Observation} =
+      jsx:decode(Body, [return_maps]),
+    #{<<"color">> := Color, <<"numbers">> := Numbers} = Observation,
+    case sequence_exists(SequenceId) of
+      {ok, found} -> process_observation_ok_json(SequenceId, Color, Numbers);
+      _           -> unknown_sequence_error_json()
+    end
+  catch
+    error:{badmatch, _} -> invalid_format_error_json()
+  end,
   Req3 = cowboy_req:set_resp_header(<<"content-type">>, <<"application/json">>, Req2),
   Req4 = cowboy_req:set_resp_body(Resp, Req3),
   {true, Req4, State}.
-  
+
 %%% Private
-process_observation_ok_json() ->
-  undefined.
-  
+process_observation_ok_json(Sequence, Color, Numbers) ->
+  [{Sequence, State}] = ets:lookup(observations, Sequence),
+  NewState = sequence_analyzer:analyze(Color, Numbers, State),
+  ets:update_element(observations, Sequence, {2, NewState}),
+  jsx:encode([{<<"status">>, <<"ok">>}, {<<"response">>, NewState}]).
+
 sequence_exists(SequenceId) ->
   case ets:member(observations, SequenceId) of
     true -> {ok, found};
     _    -> {error, not_found}
   end.
-  
+
+invalid_format_error_json() ->
+  jsx:encode([{<<"status">>, <<"error">>}, {<<"msg">>, <<"Invalid request format">>}]).
+
 unknown_sequence_error_json() ->
   jsx:encode([{<<"status">>, <<"error">>}, {<<"msg">>, <<"The sequence isn't found">>}]).
--- apps/mz/src/handlers/create_sequence_handler.erl
+++ apps/mz/src/handlers/create_sequence_handler.erl
@@ -9,29 +9,29 @@
 
 init(_Transport, _Req, []) ->
   {upgrade, protocol, cowboy_rest}.
-    
+
 content_types_accepted(Req, State) ->
   {[
     {{<<"application">>, <<"json">>, []}, from_json}
   ], Req, State}.
-    
+
 allowed_methods(Req, State) ->
   {[<<"POST">>], Req, State}.
-    
+
 from_json(Req, State) ->
   Req2 = cowboy_req:set_resp_header(<<"content-type">>, <<"application/json">>, Req),
   Resp = sequence_ok_json(),
   Req3 = cowboy_req:set_resp_body(Resp, Req2),
   {true, Req3, State}.
-  
+
 %%% Private
 sequence_ok_json() ->
   SequenceId = list_to_binary(uuid:to_string(uuid:uuid1())),
-  ets:insert(observations, {SequenceId, {}}),
+  ets:insert(observations, {SequenceId, []}),
   jsx:encode([
-    {<<"status">>, <<"ok">>}, 
-    {<<"response">>, 
+    {<<"status">>, <<"ok">>},
+    {<<"response">>,
       jsx:encode([{<<"sequence">>, SequenceId}])
     }
   ]).
-    
+
